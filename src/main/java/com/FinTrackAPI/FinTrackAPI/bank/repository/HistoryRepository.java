6f37936785ecb7a0706b2886457e13aa952d3e4f
Mateus Garcia <mateussk83@gmail.com>
2024-08-24 11:18:54 -0300

    Exclusao da tabela de historico desnecessario

--- src/main/java/com/FinTrackAPI/FinTrackAPI/bank/repository/HistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.FinTrackAPI.FinTrackAPI.bank.repository;
-
-import com.FinTrackAPI.FinTrackAPI.bank.model.entity.HistoryEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HistoryRepository extends MongoRepository<HistoryEntity, Integer> {
-}
